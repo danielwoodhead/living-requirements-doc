33435b38dc4cbabaf4b49e936004d58dfc9255c4
dan <danny2311@hotmail.com>
2024-04-08 21:16:19 +0000

    add throttling

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,17 +1,31 @@
 import { CacheModule } from '@nestjs/cache-manager';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BadgeModule } from './badges/badge.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     BadgeModule,
     CacheModule.register({ isGlobal: true }),
     ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
