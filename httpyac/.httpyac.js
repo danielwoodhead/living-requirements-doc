93b57732d892a11a20cad5588878e182954c5f58
dan <danny2311@hotmail.com>
2024-04-12 20:36:28 +0000

    run shields io in dev container

--- httpyac/.httpyac.js
+++ httpyac/.httpyac.js
@@ -2,6 +2,7 @@ module.exports = {
   environments: {
     local: {
       url: "http://localhost:3000",
+      shieldsUrl: "http://host.docker.internal:3001",
     },
     docker: {
       url: "http://host.docker.internal:3000",
