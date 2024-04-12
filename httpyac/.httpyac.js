e53ec2c3ef4ce105b7e5a4d400ebedff22ab7ee7
dan <danny2311@hotmail.com>
2024-04-12 17:25:19 +0000

    add dev instance to httpyac

--- httpyac/.httpyac.js
+++ httpyac/.httpyac.js
@@ -6,5 +6,8 @@ module.exports = {
     docker: {
       url: "http://host.docker.internal:3000",
     },
+    dev: {
+      url: "https://living-requirements.ambitiousisland-f3762769.uksouth.azurecontainerapps.io",
+    },
   },
 };
