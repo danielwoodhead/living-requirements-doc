d46f9e4bd9a318245e3ab7d34eb8846e7c455051
dan <danny2311@hotmail.com>
2024-04-14 13:18:14 +0000

    add cache-control header

--- api/src/badges/badge.controller.ts
+++ api/src/badges/badge.controller.ts
@@ -7,6 +7,7 @@ export class BadgeController {
   constructor(private readonly badgeService: BadgeService) {}
 
   @Get()
+  @Header('cache-control', 'max-age=300, s-maxage=300')
   @Header('content-type', 'image/svg+xml;charset=utf-8')
   async getBadge(@Query() request: GetBadgeRequest): Promise<StreamableFile> {
     const badge = await this.badgeService.getBadge(request);
